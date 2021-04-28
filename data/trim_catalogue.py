361f15719749f33b504c15a542d6259312691818
rcooke-ast <rcooke.ast@gmail.com>
2021-04-28 14:46:54 +0100

    trimcat

--- data/trim_catalogue.py
+++ data/trim_catalogue.py
@@ -3,24 +3,29 @@ from astropy.table import Table
 
 zmin, zmax = 2.5, 3.4
 dispreq = 2.5  # Sampling size
+avoidDLA = False
 
 # Load the catalogue
 t = Table.read('DR1_quasars_master.csv', format='ascii.csv')
 # Trim the table to only be quasars in a certain redshift range
 t_trim = t[np.where((t['zem_Adopt'].data > zmin) & (t['zem_Adopt'].data < zmax) & (t['Dispersion'].data == dispreq))]
 # Find those quasars with no DLAs in this redshift range
-nodla = t_trim['DLAzabs'].mask
-for dd in range(nodla.size):
-    if not nodla[dd]:
-        # This qso has a DLA... check the redshifts
-        include = True
-        dlaspl = t_trim['DLAzabs'].data[dd].split(",")
-        for ll in range(len(dlaspl)):
-            zabs = float(dlaspl[ll])
-            if (zabs > zmin) and (zabs<zmax):
-                include = False
-        if include:
-            nodla[dd] = True
-
-t_final = t_trim[np.where(nodla)]
+if avoidDLA:
+    nodla = t_trim['DLAzabs'].mask
+    for dd in range(nodla.size):
+        if not nodla[dd]:
+            # This qso has a DLA... check the redshifts
+            include = True
+            dlaspl = t_trim['DLAzabs'].data[dd].split(",")
+            for ll in range(len(dlaspl)):
+                zabs = float(dlaspl[ll])
+                if (zabs > zmin) and (zabs<zmax):
+                    include = False
+            if include:
+                nodla[dd] = True
+    t_final = t_trim[np.where(nodla)]
+    t_final.write('DR1_quasars_master_trimmed_nodla.csv', format='ascii.csv', overwrite=True)
+else:
+    t_final = t_trim
 t_final.write('DR1_quasars_master_trimmed.csv', format='ascii.csv', overwrite=True)
+
