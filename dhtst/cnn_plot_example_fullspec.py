fcf7ad30b61201ba5b3032ec0d2e324746d19c43
rcooke <rcooke.ast@gmail.com>
2022-02-19 21:41:55 +0000

    predict_allspectrum

--- dhtst/cnn_plot_example_fullspec.py
+++ dhtst/cnn_plot_example_fullspec.py
@@ -106,7 +106,7 @@ tst_input = ({})
 plt.plot(wave, spec, 'k-', drawstyle='steps-mid')
 plt.show()
 for pp in range(spec_len//2, spec.size-spec_len//2):
-    print(pp, spec_len//2, spec.size-spec_len//2)
+    if pp%100==0: print(pp, spec_len//2, spec.size-spec_len//2)
     tst_input['input_1'] = spec[pp-spec_len//2:pp-spec_len//2 + spec_len].reshape((1, spec_len, 1))
     tst_output = gpumodel.predict(tst_input)
     IDarr[pp] = tst_output[0].flatten()[0]
